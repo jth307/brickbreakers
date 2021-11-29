3bbc924caf8055b2b155a5f09b3cdf4caf0fd589
boo radley <booradley@boos-MacBook-Air.local>
2021-11-28 18:57:26 -0800

    create paddle

---
+++ game.js
@@ -0,0 +1,78 @@
+// SELECT CANVAS ELEMENT
+const cvs = document.getElementById("brickbreakers");
+const ctx = cvs.getContext("2d");
+
+// ADD BORDER TO CANVAS
+cvs.style.border = "4px solid #E0B0FF";
+
+// MAKE LINE THIK WHEN DRAWING TO CANVAS
+ctx.lineWidth = 2;
+
+// GAME VARIABLES AND CONSTANTS
+const PADDLE_WIDTH = 100;
+const PADDLE_MARGIN_BOTTOM = 50;
+const PADDLE_HEIGHT = 20;
+let LIFE = 3; // PLAYER HAS 3 LIVES
+let SCORE = 0;
+const SCORE_UNIT = 10;
+let LEVEL = 1;
+const MAX_LEVEL = 3;
+let GAME_OVER = false;
+let leftArrow = false;
+let rightArrow = false;
+
+// CREATE THE PADDLE
+const paddle = {
+    x : cvs.width/2 - PADDLE_WIDTH/2,
+    y : cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width : PADDLE_WIDTH,
+    height : PADDLE_HEIGHT,
+    dx :5
+}
+
+// DRAW PADDLE
+function drawPaddle(){
+    ctx.fillStyle = "#a4ede7";
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+    ctx.strokeStyle = "#000";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+// CONTROL THE PADDLE
+document.addEventListener("keydown", function(event){
+   if(event.keyCode == 37){
+       leftArrow = true;
+   }else if(event.keyCode == 39){
+       rightArrow = true;
+   }
+});
+document.addEventListener("keyup", function(event){
+   if(event.keyCode == 37){
+       leftArrow = false;
+   }else if(event.keyCode == 39){
+       rightArrow = false;
+   }
+});
+
+// MOVE PADDLE
+function movePaddle(){
+    if(rightArrow && paddle.x + paddle.width < cvs.width){
+        paddle.x += paddle.dx;
+    }else if(leftArrow && paddle.x > 0){
+        paddle.x -= paddle.dx;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
