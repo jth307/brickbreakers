bff5dd7c2a38ddacf3b5163b0b15ca6ff7ca4db2
boo radley <booradley@boos-MacBook-Air.local>
2021-11-29 00:35:18 -0800

    create ball

--- game.js
+++ game.js
@@ -12,6 +12,7 @@ ctx.lineWidth = 2;
 const PADDLE_WIDTH = 100;
 const PADDLE_MARGIN_BOTTOM = 50;
 const PADDLE_HEIGHT = 20;
+const BALL_RADIUS = 8;
 let LIFE = 3; // PLAYER HAS 3 LIVES
 let SCORE = 0;
 const SCORE_UNIT = 10;
@@ -64,15 +65,43 @@ function movePaddle(){
     }
 }
 
+// CREATE THE BALL
+const ball = {
+    x : cvs.width/2,
+    y : paddle.y - BALL_RADIUS,
+    radius : BALL_RADIUS,
+    speed : 4,
+    dx : 4 * (Math.random() * 2 - 1),
+    dy : -4
+}
+
+// DRAW THE BALL
+function drawBall(){
+    ctx.beginPath();
+
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "#e6c0cc";
+    ctx.fill();
+
+    ctx.strokeStyle = "#2e3548";
+    ctx.stroke();
+
+    ctx.closePath();
+}
+
+// MOVE THE BALL
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
 
 
+// RESET THE BALL
+function resetPositions(){
+    ball.x = cvs.width/2;
+    ball.y = paddle.y - BALL_RADIUS;
 
-
-
-
-
-
-
-
-
+    paddle.x = cvs.width/2 - PADDLE_WIDTH/2;
+    paddle.y = cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT;
+}
 
